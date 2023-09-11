3ae34d91c7d57ce4b26dd1bc3ccda4afde090489
StirlingSmith <stirlingjamessmith@gmail.com>
2023-09-11 12:12:58 +1200

    first try at working with df input

--- src/create_web_JSON.jl
+++ src/create_web_JSON.jl
@@ -6,18 +6,18 @@ import WritePostgres.user_from_id, WritePostgres.query_postgres, WritePostgres.g
 
 include("creating_nested_dicts.jl")
 
-function create_web_JSON(userID::String; date::Date=today())    
+function create_web_JSON(userID::String, df; date::Date=today())    
     user = user_from_id(userID)
 
-    df = query_postgres("processedarticles")
+    # df = query_postgres("processedarticles")
 
     j = OrderedDict()
 
-    j["customer_id"]=user[:id] 
+    j["customer_id"]=userID
 
-    j["customer_name"]=user[:name]
+    #j["customer_name"]=user[:name]
 
-    j["customer_information"]=user[:information]
+    #j["customer_information"]=user[:information]
 
     j["data"] = create_data_dict(df, user, date)
 
