b0ae909a9610442a97a128aa8de8f2ead91476b5
StirlingSmith <stirlingjamessmith@gmail.com>
2023-10-10 13:54:07 +1300

    update for IOM_WritePostgres (name had changed)

--- src/IOM_MakeWebJSON.jl
+++ src/IOM_MakeWebJSON.jl
@@ -1,15 +1,23 @@
 module IOM_MakeWebJSON
+
     using JSON
     using OrderedCollections
     using DataFrames
     using Dates
     using LibPQ
     using TimeZones
-    using WritePostgres
+    using IOM_WritePostgres
     using Languages
 
-    include("creating_nested_dicts.jl")
     include("create_web_JSON.jl")
+
+    include("data_dict.jl")
+
+    include("nestedDictionaryFuncs/col_dict.jl")
+    include("nestedDictionaryFuncs/keywords_dict.jl")
+    include("nestedDictionaryFuncs/news_dict.jl")
+    include("nestedDictionaryFuncs/word_dict.jl")
+
 
     export create_web_JSON
 end
